0a5a6297d2b8c5e1b1445982cea2014908a508c3
Murillo Isidoro de Medeiros <murillo.idem@gmail.com>
2022-07-11 07:12:48 -0300

    :sparkles: New: add service for deleteCategory

--- src/infra/http/routes/category.ts
+++ src/infra/http/routes/category.ts
@@ -1,17 +1,21 @@
 import { Router } from "express";
 import { body } from "express-validator";
 import { CreateCategoryController } from "../../../modules/Book/useCases/createCategory/CreateCategoryController";
+import { DeleteCategoryController } from "../../../modules/Book/useCases/deleteCategory/DeleteCategoryController";
 import { ListAllCategoryController } from "../../../modules/Book/useCases/listAllCategory/ListAllCategoryController";
 import { UpdateCategoryController } from "../../../modules/Book/useCases/updateCategory/UpdateCategoryController";
+import { ensureAuthenticationAdmin } from "../middlewares/EnsureAuthenticationAdmin";
 
 const categoryRouter = Router();
 
 const createCategoryController = new CreateCategoryController();
 const updateCategoryController = new UpdateCategoryController();
 const listAllCategoryController = new ListAllCategoryController();
+const deleteCategoryController = new DeleteCategoryController();
 
 categoryRouter.post(
   "/",
+  ensureAuthenticationAdmin,
   body('name').isString().notEmpty().withMessage("name is string and cannot be empty."),
   body('description').isString().notEmpty().withMessage("description is string and connot be empty."),
   createCategoryController.handle
@@ -19,6 +23,7 @@ categoryRouter.post(
 
 categoryRouter.put(
   "/:id",
+  ensureAuthenticationAdmin,
   body('name').isString().notEmpty().withMessage("name is string and cannot be empty."),
   body('description').isString().notEmpty().withMessage("description is string and connot be empty."),
   updateCategoryController.handle
@@ -26,7 +31,14 @@ categoryRouter.put(
 
 categoryRouter.get(
   "/",
+  ensureAuthenticationAdmin,
   listAllCategoryController.handle
+);
+
+categoryRouter.delete(
+  "/:id",
+  ensureAuthenticationAdmin,
+  deleteCategoryController.handle
 );
 
 export { categoryRouter };
